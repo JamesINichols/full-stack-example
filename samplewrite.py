37841c0f890d832fe5e4bfe81b73caef3ad7ac91
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2023-09-29 16:03:56 -0400

    Add files via upload

---
+++ samplewrite.py
@@ -0,0 +1,26 @@
+def write_file():
+    print("Writing a file..")
+    try:
+        f = open("my_file.txt", "w")
+        for num in range(5):
+            f.write("Line " + str(num) + "\n")
+        f.close()
+    except Exception:
+        print("Could not write to file")
+
+
+def read_file():
+    print("Now reading the file..")
+    try:
+        f = open("my_file.txt", "r")
+        for line in f.readlines():
+            print(line)
+        f.close()
+    except Exception:
+        print("Could not read to file")
+
+def main():
+    write_file()
+
+    read_file()
+
